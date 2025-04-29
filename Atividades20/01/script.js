f7252b9fd05afbebd8ce5685f11868566df1a7c1
Victor Gomes <victor.estudos3592@gmail.com>
2025-04-29 15:37:37 -0300

    Ignorar Atividades20 e script.js

--- Atividades20/01/script.js
+++
@@ -1,5 +0,0 @@
-alert("Bem-vindo! A seguir")
-let nome = prompt("Qual seu nome?: ")
-let idade = Number(prompt("Qual sua idade"))
-let confirmacao = confirm(nome + " Sua idade " + idade + " está correta ?")
-let exibir = alert(`Nome: ${nome}\n Idade: ${idade}\n Confirmação: ${confirmacao}`)
