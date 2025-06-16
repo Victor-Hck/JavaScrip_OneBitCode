c5ab60138cdf779a080bc19a58efdd343ba3b241
¨Victor <victor.profissional3592@gmail.com>
2025-06-16 14:47:09 -0300

    ¨update¨

--- JavaScript III/exercicios/01/script.js
+++ JavaScript III/exercicios/01/script.js
@@ -1,15 +1,16 @@
 function addPlayer() {
     const position = document.getElementById("position").value
-     const name = document.getElementById("name").value
+    const name = document.getElementById("name").value
     const number = document.getElementById("number").value
 
-    const confirmation = confirm("Escalar " + name + " como " + position + "?")
+    // const confirmation = confirm("Escalar " + name + " como " + position + "?")
+    const confirmation = confirm(`Escalar ${name} como ${position} ?`)
 
     if (confirmation) {
         const teamList = document.getElementById("team-list")
         const playerItem = document.createElement("li")
         playerItem.id = "player-" + number
-        playerItem.innerText = position + ": " + name + " (" + number + ")"
+        playerItem.innerText = `${position} : ${name} (${number})`
         teamList.appendChild(playerItem)
 
         document.getElementById("position").value = ""
